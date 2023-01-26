b737f71872ea98aad068fe7673b872f38dc5f616
corneliusphanthanh <cornelius.phanthanh@pnmac.com>
2023-01-26 14:22:10 +0900

    Snapshot testing

---
+++ src/__tests__/app.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+
+test('renders header', () => {
+  render(<App />);
+  const header = screen.getByRole('heading');
+  expect(header).toBeInTheDocument();
+});
+
+test('renders body container', async () => {
+  render(<App />);
+  const body = screen.getByTestId('body');
+  expect(body).toBeInTheDocument();
+  
+});
+
